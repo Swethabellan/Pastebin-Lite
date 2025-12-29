d910b482e4b6d7f8192a9ed3909360f503a41498
swetha <swethabellan@gmail.com>
2025-12-29 16:32:11 +0530

    fix

---
+++ frontend/prisma/migrations/20251229091732_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Paste" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "ttlSeconds" INTEGER,
+    "maxViews" INTEGER,
+    "remainingViews" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Paste_pkey" PRIMARY KEY ("id")
+);
